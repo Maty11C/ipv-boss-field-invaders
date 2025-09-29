72cb9ad36fcabc96d612182d6ab78b97fb0dc888
Maty11C <matiasrodrigocabrera@gmail.com>
2025-09-29 20:03:19 -0300

    Se actualiza proyecto a godot 4.5

--- src/ui/hud.gd
+++ src/ui/hud.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 
 @onready var score_label: Label = $ScoreLabel
+@onready var play_button: Button = $PlayButton
+@onready var main_menu_button: Button = $MainMenuButton
 @onready var music: AudioStreamPlayer2D = $Music
 @onready var play_sound: AudioStreamPlayer2D = $PlaySound
 
@@ -12,21 +14,19 @@ func _ready() -> void:
 func update_score(score):
 	score_label.text = "Score: %d" % [score]
 
-
 func _on_play_button_pressed() -> void:
-	$PlaySound.play()
-	$PlayButton.hide()
-	$MainMenuButton.hide()
+	play_sound.play()
+	play_button.hide()
+	main_menu_button.hide()
 	music.stop()
 	start_game.emit()
-
+	main_menu_button.hide()
 
 func _on_main_open_loser_hud() -> void:
-	$PlayButton.text = "PLAY AGAIN"
-	$PlayButton.show()
-	$MainMenuButton.show()
-
+	play_button.text = "PLAY AGAIN"
+	play_button.show()
+	main_menu_button.show()
 
 func _on_main_menu_button_pressed() -> void:
-	$PlayButton.text = "PLAY"
-	$MainMenuButton.hide()
+	play_button.text = "PLAY"
+	main_menu_button.hide()
